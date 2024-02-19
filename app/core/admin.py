71a0e22272c2415e8bcfa1c42dc87eec80828671
Rommel de Torres <detorresrc@gmail.com>
2024-02-19 15:19:11 +0800

    - User Endpoint

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,47 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core.models import User
+from django.utils.translation import gettext as _
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name', 'is_superuser', 'is_active']
+    readonly_fields = ['last_login']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name',)}),
+        (_('Permissions'), {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2'
+            )
+        }),
+        (_('Personal info'), {
+            'fields': ('name',),
+            'classes': ('collapse', 'wide'),
+        }),
+        (_('Permissions'), {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            ),
+            'classes': ('collapse', 'wide')
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
