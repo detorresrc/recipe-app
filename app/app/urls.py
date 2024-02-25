4131e157bf9b19c1581b194b6260e8f84a40b662
Rommel de Torres <detorresrc@gmail.com>
2024-02-25 11:15:34 +0800

    Recipe Image Upload

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -20,3 +22,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -35,7 +35,8 @@ class RecipeSerializer(serializers.ModelSerializer):
             'price',
             'link',
             'tags',
-            'ingredients'
+            'ingredients',
+            'image'
         ]
         read_only_fields = ['id']
 
@@ -92,3 +93,13 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """ Serializer for uploading images to recipes. """
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
